d95a1ed420c6f0ba227f958f5538379cd7bc7931
Jsinha19 <jahnvi.sinha19@gmail.com>
2025-09-07 23:17:08 +0530

    url update

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { AuthResponse, Item, ItemsResponse, Cart, FilterParams } from '../types';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://ecommerce-backend-spdi.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
